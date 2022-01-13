ce97f72ff90c6207b5e795fa76a108a17b3b58c8
Lingling <97364813+Lingling-LynnChan@users.noreply.github.com>
2022-01-14 07:52:57 +0800

    Update AvlTree.c
    Fixed some bugs.The addition routine has been improved.Improved code readability.

--- AvlTree.c
+++ AvlTree.c
@@ -6,134 +6,10 @@ typedef Node* BinTree;
 
 struct TNode {
 	int Data;
-	int Height;
 	BinTree Left;
 	BinTree Right;
 };
 
-int
-max (int x,int y) {
-	return x>y?x:y;
-}
-int
-height (BinTree AVL) {
-	if (!AVL) {
-		return -1;
-	}else {
-		return AVL->Height;
-	}
-}
-BinTree
-singleLeft (BinTree AVL) {
-	BinTree P=AVL->Left;
-	AVL->Left=P->Right;
-	P->Right=AVL;
-	AVL->Height=max(height(AVL->Left),height(AVL->Right))+1;
-	P->Height=AVL->Height;
-	return P;
-}
-BinTree
-singleRight (BinTree AVL) {
-	BinTree P=AVL->Right;
-	AVL->Right=P->Left;
-	P->Left=AVL;
-	AVL->Height=max(height(AVL->Left),height(AVL->Right))+1;
-	P->Height=AVL->Height;
-	return P;
-}
-BinTree
-doubleLeft (BinTree AVL) {
-	AVL->Left=singleRight(AVL->Left);
-	return singleLeft(AVL);
-}
-BinTree
-doubleRight (BinTree AVL) {
-	AVL->Right=singleLeft(AVL->Right);
-	return singleRight(AVL);
-}
-BinTree
-insert (BinTree AVL,int X) {
-	if (!AVL) {
-		AVL=(BinTree)malloc(sizeof(Node));
-		AVL->Height=0;
-		AVL->Data=X;
-		AVL->Left=AVL->Right=NULL;
-		return AVL;
-	}
-	if (AVL->Data>X) {
-		AVL->Left=insert(AVL->Left,X);
-		if (height(AVL->Left)-height(AVL->Right)>1) {
-			if (AVL->Left->Data>X) {
-				AVL=singleLeft(AVL);
-			}else {
-				AVL=doubleLeft(AVL);
-			}
-		}
-	}else
-		if (AVL->Data<X) {
-			AVL->Right=insert(AVL->Right,X);
-			if (height(AVL->Right)-height(AVL->Left)>1) {
-				if (AVL->Right->Data<X) {
-					AVL=singleRight(AVL);
-				}else {
-					AVL=doubleRight(AVL);
-				}
-			}
-		}
-	AVL->Height=max(height(AVL->Left),height(AVL->Right));
-	return AVL;
-}
-BinTree
-minTree (BinTree AVL) {
-	if (AVL->Left) {
-		return minTree(AVL->Left);
-	}
-	return AVL;
-}
-BinTree
-maxTree (BinTree AVL) {
-	if (AVL->Right) {
-		return maxTree(AVL->Right);
-	}
-	return AVL;
-}
-BinTree
-delete (BinTree AVL,int X) {
-	if (!AVL) {
-		return NULL;
-	}
-	BinTree Del;
-	if (AVL->Data>X) {
-		AVL->Left=delete(AVL->Left,X);
-	}else
-		if (AVL->Data<X) {
-			AVL->Right=delete(AVL->Right,X);
-		}else {
-			if (AVL->Left&&AVL->Right) {
-				Del=minTree(AVL->Right);
-				AVL->Data=Del->Data;
-				AVL->Right=delete(AVL->Right,Del->Data);
-			}else {
-				Del=AVL;
-				if (!AVL->Left) {
-					AVL=AVL->Right;
-				}else {
-					AVL=AVL->Left;
-				}
-				free(Del);
-			}
-		}
-	return AVL;
-}
-void
-print(BinTree AVL) {
-	if (!AVL) {
-		return;
-	}
-	print(AVL->Left);
-	printf("%d ",AVL->Data);
-	print(AVL->Right);
-}
 void
 printLevelTools (BinTree AVL,int level) {
 	int i;
@@ -155,19 +31,127 @@ void
 printLevel (BinTree AVL) {
 	printLevelTools(AVL,0);
 }
+int
+max (int x,int y) {
+	return x>y?x:y;
+}
+int
+getHeight (BinTree AVL) {
+	if (!AVL) {
+		return -1;
+	}
+	return max(getHeight(AVL->Left),getHeight(AVL->Right))+1;
+}
+BinTree
+minTree (BinTree AVL) {
+	if (AVL->Left) {
+		return minTree(AVL->Left);
+	}
+	return AVL;
+}
+BinTree
+maxTree (BinTree AVL) {
+	if (AVL->Right) {
+		return maxTree(AVL->Right);
+	}
+	return AVL;
+}
+BinTree
+newTree (void) {
+	return NULL;
+}
+BinTree
+newNode (int X) {
+	BinTree AVL=(BinTree)malloc(sizeof(Node));
+	AVL->Data=X;
+	AVL->Left=AVL->Right=NULL;
+	return AVL;
+}
+BinTree
+LL (BinTree AVL) {
+	if (AVL) {
+		BinTree P=AVL->Left;
+		AVL->Left=P->Right;
+		P->Right=AVL;
+		AVL=P;
+	}
+	return AVL;
+}
+BinTree
+RR (BinTree AVL) {
+	if (AVL) {
+		BinTree P=AVL->Right;
+		AVL->Right=P->Left;
+		P->Left=AVL;
+		AVL=P;
+	}
+	return AVL;
+}
+BinTree
+LR (BinTree AVL) {
+	if (AVL) {
+		AVL->Left=RR(AVL->Left);
+		AVL=LL(AVL);
+	}
+	return AVL;
+}
+BinTree
+RL (BinTree AVL) {
+	if (AVL) {
+		AVL->Right=LL(AVL->Right);
+		AVL=RR(AVL);
+	}
+	return AVL;
+}
+/*Upside is Tools*/
+BinTree
+addTree (BinTree AVL,int X) {
+	if (!AVL) {
+		return newNode(X);
+	}
+	if (AVL->Data>X) {
+		AVL->Left=addTree(AVL->Left,X);
+		if (getHeight(AVL->Left)-getHeight(AVL->Right)>1) {
+			if (AVL->Left->Data>X) {
+				AVL=LL(AVL);
+			}else {
+				AVL=LR(AVL);
+			}
+		}
+	}else
+		if (AVL->Data<X) {
+			AVL->Right=addTree(AVL->Right,X);
+			if (getHeight(AVL->Right)-getHeight(AVL->Left)>1) {
+				if (AVL->Right->Data<X) {
+					AVL=RR(AVL);
+				}else {
+					AVL=RL(AVL);
+				}
+			}
+		}
+	/*Adjust the balance*/
+	return AVL;
+}
+BinTree
+delTree (BinTree AVL,int X) {
+	
+}
+
 
 int
 main (int argc, char *argv[]) {
 	int n,x;
-	BinTree AVL=NULL;
+	BinTree AVL=newTree();
 	scanf("%d",&n);
 	while (n--) {
 		scanf("%d",&x);
-		AVL=insert(AVL,x);
+		AVL=addTree(AVL,x);
 	}
 	printLevel(AVL);
-	scanf("%d",&x);
-	AVL=delete(AVL,x);
-	printLevel(AVL);
+//	scanf("%d",&x);
+//	AVL=delTree(AVL,x);
+//	printLevel(AVL);
+//	AVL=delAllTree(AVL);
+//	printLevel(AVL);
 	return 0;
 }
